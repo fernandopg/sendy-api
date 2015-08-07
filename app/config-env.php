ff418c31f951914946a507e82f85acacdf7f71d7
Fernando Pascua García <fernando.pascua.garcia@gmail.com>
2015-08-07 13:32:59 +0200

    add config file

---
+++ app/config-env.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by: gellu
+ * Date: 12.09.2013 16:34
+ */
+
+# copy this file to config-env.php
+
+$configEnv = [
+	'pdo'	=> array(
+		'host'	=> 'aa1243r3fy4jfdh.czylzwek9psv.us-east-1.rds.amazonaws.com',
+		'port'	=> '3306',
+		'name'	=> 'sendy',
+		'user'	=> 'ebroot',
+		'pass'	=> 'n0t0r10us1m'
+	),
+];
