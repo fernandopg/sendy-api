0d39c994c48a201cf1714979b392dc08a573f4ae
Fernando Pascua García <fernando.pascua.garcia@gmail.com>
2015-08-13 12:59:08 +0200

    config dev

--- app/config-env.php
+++ app/config-env.php
@@ -5,7 +5,7 @@
  */
 
 # copy this file to config-env.php
-
+/*
 $configEnv = [
 	'pdo'	=> array(
 		'host'	=> 'aa1243r3fy4jfdh.czylzwek9psv.us-east-1.rds.amazonaws.com',
@@ -13,5 +13,15 @@ $configEnv = [
 		'name'	=> 'sendy',
 		'user'	=> 'ebroot',
 		'pass'	=> 'n0t0r10us1m'
+	),
+];
+ */
+$configEnv = [
+	'pdo'	=> array(
+		'host'	=> '127.0.0.1',
+		'port'	=> '3306',
+		'name'	=> 'sendy',
+		'user'	=> 'root',
+		'pass'	=> 'theboss14'
 	),
 ];
